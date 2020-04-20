31b4b146effc239b80da365177d5e072ac8a4db3
GeorgeCoops <coopsgeorge@gmail.com>
2020-04-20 18:06:06 +0100

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(str) {
+  const time = parseInt(str.split(':')[0])
+  if (time < 12){
+    return "Good Morning"
+  }
+  else if (time < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(str) {
+  document.querySelector('#greeting').innerText = str
+}
